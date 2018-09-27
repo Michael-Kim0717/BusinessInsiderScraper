058aa0ee77bd5773c0b7d62d68771d6f6d607341
Michael Kim <MichaelKim3@hotmail.com>
2018-09-27 16:54:14 -0400

    Added commenting, Changed models to reflect Mongoose Schema

--- public/app.js
+++ public/app.js
@@ -1,19 +1,11 @@
 $(document).ready(function(){
 
     // Grab the headlines and images as a json
+    // Add all received articles into the homepage
     $.getJSON("/all", function(data) {
         for (var i = 0; i < data.length; i++) {
-            if (i == 0){
-                $(".collapse").append(
-                    "<input type='radio' id=" + data[i]._id + " checked aria-hidden='true' name='articleAcc'>"
-                );
-            }
-            else {
-                $(".collapse").append(
-                    "<input type='radio' id=" + data[i]._id + " aria-hidden='true' name='articleAcc'>"
-                );
-            }
             $(".collapse").append(
+                "<input type='radio' class='articleAcc' id=" + data[i]._id + " aria-hidden='true' name='articleAcc'>" +
                 "<label for=" + data[i]._id + " aria-hidden='true'> <b>" + data[i].title + " </b> </label>" +
                 "<div>" +
                     "<p> Link to Article : " + 
@@ -23,22 +15,75 @@ $(document).ready(function(){
                         "<a href=" + "#" + "> Comments </a>" +
                     "</p>" +
                     "<img src=" + data[i].img + " class='articleImg'>" +
-                    "<input type='text' placeholder='Name'> <br>" +
-                    "<textarea> </textarea>" +
-                    "<button class='btn'> Submit Comment </button>" +
-                    "<div class=" + "comments" + data[i]._id + ">" +
-                        "<h3> Comments : </h3>" +
+                    "<input type='text' placeholder='Name' id=" + "name" + data[i]._id + "> <br>" +
+                    "<textarea id=" + "comment" + data[i]._id + "> </textarea>" +
+                    "<button class='btn submitComment' id=" + "submit" + data[i]._id + "> Submit Comment </button>" +
+                    "<h3> Comments : </h3>" +
+                    "<div class='comments " + "comments" + data[i]._id + "'>" +
                     "</div>" +
                 "</div>"
             );
         }
     });
 
-    if (window.location.href == "/info"){
-        alert("hello");
-        setTimeout(function(){
-            window.location.href = "/";
-        }, 2000);
-    }
+    // Whenever the user clicks on a collapsible article,
+    // Grab the comments and populate the specific article.
+    $(document).on("click", ".articleAcc", function() {
+        var id = $(this).attr("id");
+
+        $(".comments" + id).empty();
+
+        $.ajax({
+            method: "GET",
+            url: "/all/" + id
+        })
+        .then(function(data){
+            for (var i = 0; i < data.comment.length; i++){
+                $(".comments" + id).append(
+                    "<h5>" + data.comment[i].name + "</h5>" +
+                    "<h6>" + data.comment[i].comment + "</h6>" +
+                    "<hr>"
+                );
+            }
+        })
+    });
+
+    // Whenever the user submits a comment to a post,
+    // Grab the name and comment field
+    // Call an AJAX request to add the comment into that specific post.
+    $(document).on("click", ".submitComment", function(){
+        var id = $(this).attr("id").substring(6);
+        var name = $("#name" + id).val().trim();
+        var comment = $("#comment" + id).val().trim();
+
+        $.ajax({
+            method: "POST",
+            url: "/all/" + id,
+            data: {
+                name: name,
+                comment: comment
+            }
+        })
+        .then(function(data){
+            $("#name" + id).val("");
+            $("#comment" + id).val("");
+            
+            $(".comments" + id).empty();
+
+            $.ajax({
+                method: "GET",
+                url: "/all/" + id
+            })
+            .then(function(data){
+                for (var i = 0; i < data.comment.length; i++){
+                    $(".comments" + id).append(
+                        "<h5>" + data.comment[i].name + "</h5>" +
+                        "<h6>" + data.comment[i].comment + "</h6>" +
+                        "<hr>"
+                    );
+                }
+            })
+        })
+    })
 
 });
--- server.js
+++ server.js
@@ -14,17 +14,10 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bIScraper";
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
-
-// Configure database
-var databaseUrl = "bIScraper";
-var collections = ["businessInsider"];
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });;
 
 // Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+var db = require("./models");
 
 // Default PORT to be used
 const PORT = 3000;
@@ -38,21 +31,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Retrieve data from the db
+// Retrieve data from the db to be used to populate the home page
 app.get("/all", function(req, res) {
     // Find all results from the scrapedData collection in the db
-    db.businessInsider.find({}, function(error, found) {
-        // Throw any errors to the console
-        if (error) {
-            console.log(error);
-        }
-        // If there are no errors, send the data to the browser as json
-        else {
-            res.json(found);
-        }
-    });
+    db.Article.find({})
+        .then(function(dbArticle){
+            res.json(dbArticle);
+        })
+        .catch(function(error){
+            res.json(error);
+        });
 });
 
+// Retrieve data from the Business Insider website in order to populate the database
 app.get("/info", function(req, res) {
     // Making a request for Business Insider's front page. The page's HTML is passed as the callback's third argument
     request("https://www.businessinsider.com/", function(error, response, html) {
@@ -67,33 +58,61 @@ app.get("/info", function(req, res) {
             var title = $(element).text();
             var link = $(element).attr("href");
             var img = $(element).parent().parent().find("img").attr("src");
+
+            var result = {
+                title: title,
+                link: link,
+                img: img
+            }
   
             // If this found element had both a title and a link
             if (img != undefined) {
                 // Insert the data in the scrapedData db
-                console.log(title + ", " + link);
-                db.businessInsider.update(
-                    {title : title},
-                    {$set:{"link": link, "img": img}},
-                    {upsert: true},
-                function(err, inserted) {
-                    if (err) {
-                        // Log the error if one is encountered during the query
-                        console.log(err);
-                    }
-                    else {
-                        // Otherwise, log the inserted data
-                        console.log(inserted);
-                    }
-                });
+                db.Article.create(result)
+                    .then(function(dbArticle){
+                        console.log(dbArticle);
+                    })
+                    .catch(function(error){
+                        return res.json(error);
+                    });
             }
         });
     });
-  
-    // Send a "Scrape Complete" message to the browser
+
+    // Once the data has been received, go back to the home page
     res.redirect("/");
 });
-  
+
+// Grab the specific Article and its data when the headline is clicked
+app.get("/all/:id", function(request, response){
+    db.Article.findOne({ _id: request.params.id })
+        .populate("comment")
+        .then(function(dbArticle) {
+            response.json(dbArticle);
+        })
+        .catch(function(error){
+            response.json(error);
+        });
+});
+
+// When the user clicks the submit comment button,
+// Add the comment onto the database
+app.post("/all/:id", function(request, response){
+    db.Comment.create(request.body)
+        .then(function(dbComment){
+            return db.Article.findOneAndUpdate(
+                { _id : request.params.id },
+                { $push: { comment: dbComment._id } },
+                { new: true }
+            )
+        })
+        .then(function(dbArticle) {
+            response.json(dbArticle);
+        })
+        .catch(function(error){
+            response.json(error);
+        });
+});
 
 // Start the server
 app.listen(PORT, function() {
